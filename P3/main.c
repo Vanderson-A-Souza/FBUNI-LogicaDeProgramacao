48a9d2b56d1ac14dc23731610b3d0e2ec5ebde95
Vanderson A. Souza <vandersonandrades@outlook.com>
2019-08-27 10:58:41 -0300

    Projeto 3

---
+++ P3/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+// Inicio do Programa
+	
+	//Declaração de vaviavel
+	float cocada, qqueixo, valorTotal;
+	int qtCocada, qtQqueixo;
+	===     === 	===		 ====    ==
+	 ==     ==	   == ==	 == ==   ==
+	  ==   ==	  ==   ==	 ==  ==  == 
+	   == ==	 =========	 ==   == ==
+	    ===		===     ===  ==	   ====
+	//Recebendo valores para cocada.
+	printf("Preco cocada: R$");
+	scanf("%f", &cocada);
+	printf("Quantidade: ");
+	scanf("%d", &qtCocada);
+	//Recebendo valores para o quebra-queixo.
+	printf("Preco quebra-queixo: R$");
+	scanf("%f", &qqueixo);
+	printf("Quantidade: ");
+	scanf("%d", &qtQqueixo);
+	
+	//Calculando o valor total a ser pago.
+	valorTotal = cocada*qtCocada + qqueixo*qtQqueixo;
+	printf("Total a pagar: R$%.2f", valorTotal);
+	
+	return 0;
+}
